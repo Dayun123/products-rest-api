d104a579518620fc5b704dd33f660d33d96537fa
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-11-18 13:29:38 -0600

    Uppercase the first letter of the product name in 'create-products.js'.
    The faker.hacker.noun() method returns a string with a lowercase
    first letter. As I'm using this as the first word of the product
    name, I want that letter to be uppercase. It seems like there should
    be a standard JS method for this, but there doesn't seem to be.

--- data/create-products.js
+++ data/create-products.js
@@ -4,9 +4,11 @@ const products = [];
 const numProducts = 1000;
 
 for (let i = 1; i <= numProducts; i++) {
+  let str = faker.hacker.noun();
+  str = str[0].toUpperCase() + str.slice(1, str.length - 1);
   products.push({
     id: i,
-    name: `${faker.hacker.noun()} ${faker.commerce.product()}`,
+    name: `${str} ${faker.commerce.product()}`,
     price: faker.commerce.price(),
     category: faker.commerce.department(),
   });
