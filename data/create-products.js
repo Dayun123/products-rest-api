2d085f38403ed3357c2e95e076f2c02cbb4954f9
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-11-18 13:20:23 -0600

    Change the loop-iteration variable in 'create-products.js'.
    I'm using the loop-iteration variable to set the id on each
    product. Was originally starting it at 0, but I would rather the
    id's start at 1.

--- data/create-products.js
+++ data/create-products.js
@@ -3,7 +3,7 @@ const faker = require('faker');
 const products = [];
 const numProducts = 1000;
 
-for (let i = 0; i < numProducts; i++) {
+for (let i = 1; i <= numProducts; i++) {
   products.push({
     id: i,
     name: `${faker.hacker.noun()} ${faker.commerce.product()}`,
