43471f870a028983594b0a4fb8989ca8fc5ae2bc
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-11-20 15:35:08 -0600

    Move logic for finding a product by :id to the router.param('id') middleware.

--- routes/products.js
+++ routes/products.js
@@ -5,13 +5,18 @@ const validate = require('../utils/validate');
 const router = express.Router();
 
 router.param('id', (req, res, next) => {
+
   res.locals.id = +req.params.id;
+  
   if (!validate.validateId(res.locals.id)) {
     next({
       statusCode: 422,
       statusMessage: ':id parameter must be a number and must be greater than 0',
     });
   }
+  
+  res.locals.product = products.find((product) => product.id === res.locals.id);
+  
   next();
 });
 
@@ -31,14 +36,14 @@ router.post('/', validate.validateContentType, validate.validateProductKeys, (re
 });
 
 router.get('/:id', (req, res, next) => {
-  const product = products.find((product) => product.id === res.locals.id);
-  if (!product) {
+  
+  if (!res.locals.product) {
     next({
       statusCode: 404,
       statusMessage: 'No product found with that id'
     });
   } else {
-    res.json(product);
+    res.json(res.locals.product);
   }
 });
 
