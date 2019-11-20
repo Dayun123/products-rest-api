d4f5738f03de5da9428ce99fa63cff7b450a5497
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-11-19 18:03:47 -0600

    Add GET /products and return a test JSON response.
    This route will eventually return all the products, but for now
    just sending back a JSON object to test that it is working.

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 
+const productsRouter = require('./routes/products');
+
 var app = express();
+
 
 app.use(logger('dev'));
 app.use(express.json());
+
+app.use('/products', productsRouter);
 
 module.exports = app;
